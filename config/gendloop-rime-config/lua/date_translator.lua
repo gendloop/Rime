c1c46a54188fe04ba05bab3fdc0693eee3ff97a6
gendloop <gendloop.cn@gmail.com>
2025-08-01 11:34:20 +0800

    chore: update

--- config/gendloop-rime-config/lua/date_translator.lua
+++ config/gendloop-rime-config/lua/date_translator.lua
@@ -5,6 +5,7 @@ local function date_translator(input, seg)
        yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), "  日期"))
        yield(Candidate("date", seg.start, seg._end, os.date("%Y/%m/%d/"), "  日期"))
        yield(Candidate("date", seg.start, seg._end, os.date("%Y_%m_%d"), "  日期"))
+       yield(Candidate("date", seg.start, seg._end, os.date("%Y%m%d%H%M"), "  日期"))
     end
 end
 
